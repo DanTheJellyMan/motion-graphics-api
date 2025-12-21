c1710b3088b52a948fc428f92db7b1bfa72eced6
DanTheJellyMan <d3362719@gmail.com>
2025-12-21 07:14:41 -0600

    Major GraphicNode redesign
    GraphicNodes now store objects and have their own querying methods for children. This is to make the design more simple to understand, rather than alternatively having actual SVG and other elements directly stored by GraphicNodes

--- web/public/script.js
+++ web/public/script.js
@@ -2,28 +2,32 @@ import Graphic from "/lib/Graphic.js";
 import GraphicNode from "/lib/GraphicNode.js";
 
 const ctx = document.querySelector("#preview-container > canvas").getContext("2d");
+const designer = document.querySelector("#graphic-designer");
 
 const graphic = new Graphic(10, 200, 200, Infinity, 30);
-const node = new GraphicNode(200, 200, 100);
-const nodeSvg = node.getSvg();
-const path = document.createElementNS(GraphicNode.svgNS, "path");
-path.setAttribute("d", "M0 0 L200 200 L0 200 Z");
-path.setAttribute("fill", "hotpink");
-path.setAttribute("stroke", "lime");
-path.setAttribute("stroke-width", "5");
-nodeSvg.appendChild(path);
+const parentNode = new GraphicNode("svg");
+parentNode.addKeyframe({
+    t: 0,
+    attribs: {
+        width: 200,
+        height: 200
+    }
+});
+const node = new GraphicNode("path");
+node.addKeyframe({
+    t: 0,
+    attribs: {
+        "d": "M0 0 L200 200 L0 200 Z",
+        "fill": "hotpink",
+        "stroke": "lime",
+        "stroke-width": "5"
+    }
+});
+parentNode.appendChild(node);
 
-const node2 = new GraphicNode(200, 200, 500);
-const node2Svg = node2.getSvg();
-const path2 = document.createElementNS(GraphicNode.svgNS, "path");
-path2.setAttribute("d", "M50 0 L250 200 L50 200 Z");
-path2.setAttribute("fill", "hotpink");
-path2.setAttribute("stroke", "lime");
-path2.setAttribute("stroke-width", "20");
-node2Svg.appendChild(path2);
+document.body.appendChild(parentNode.generateElement());
+// graphic.addNode(node);
 
-graphic.addNode(node);
-graphic.addNode(node2);
 // graphic.render("GIF").then((blob) => {
 //     const gifUrl = URL.createObjectURL(blob);
 //     if (window.confirm("Download rendered GIF?")) {
@@ -36,23 +40,3 @@ graphic.addNode(node2);
 //     }
 //     URL.revokeObjectURL(gifUrl);
 // });
-
-// setInterval(() => {
-//     const { width, height } = ctx.canvas;
-//     const lineWidth = 10;
-//     const lineHeight = 20;
-//     const gap = 10;
-//     let x = (lineWidth/2) * Math.sin(Math.sqrt((performance.now()/10) * Math.sqrt(performance.now()))) - (width/4);
-
-//     ctx.clearRect(0, 0, width, height);
-//     ctx.fillStyle = "orange";
-//     while (x < width) {
-//         ctx.fillRect(
-//             x,
-//             height/2 - (lineHeight/2),
-//             lineWidth,
-//             lineHeight
-//         );
-//         x += lineWidth + gap;
-//     }
-// }, 1000 / 165);
