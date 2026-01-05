a98a0addd8f5f366af73e012a7672a4e3cdd2721
DanTheJellyMan <d3362719@gmail.com>
2026-01-04 20:58:34 -0600

    Full support for SVG rendering added

--- web/public/script.js
+++ web/public/script.js
@@ -3,12 +3,13 @@ import GraphicNode from "/lib/GraphicNode.js";
 
 const ctx = document.querySelector("#preview-container > canvas").getContext("2d");
 const designer = document.querySelector("#graphic-designer");
-const GRAPHIC_WIDTH = 200;
-const GRAPHIC_HEIGHT = 200;
+const GRAPHIC_WIDTH = 1000;
+const GRAPHIC_HEIGHT = 1000;
+const GRAPHIC_BG_COLOR = "hsl(300,0%,30%)";
 
-const graphic = new Graphic(GRAPHIC_WIDTH, GRAPHIC_HEIGHT, Infinity, 2);
-graphic.setGifFps(30);
-graphic.setGifEncoderSettings(Infinity, 10, true, 1, "#0f0");
+const graphic = new Graphic(GRAPHIC_WIDTH, GRAPHIC_HEIGHT, Infinity, 1.5);
+graphic.setGifFps(45);
+graphic.setGifEncoderSettings(10, false, navigator.hardwareConcurrency, GRAPHIC_BG_COLOR);
 graphic.setSvgViewBox(`0 0 ${GRAPHIC_WIDTH} ${GRAPHIC_HEIGHT}`);
 
 const parentNode = new GraphicNode("svg");
@@ -19,46 +20,99 @@ parentNode.addKeyframe({
         "height": GRAPHIC_HEIGHT
     }
 });
-const node = new GraphicNode("path");
+
+const node = new GraphicNode("rect");
 node.addKeyframe({
     t: 0,
     attribs: {
-        "d": "M0,0 L200,200 L0,200 Z",
-        "fill": "hotpink",
-        "stroke": "lime",
-        "stroke-width": "5"
-    }
-});
-node.addKeyframe({
-    t: 0.5,
-    attribs: {
-        "d": "M100,0 L50,150 L50,100Z",
-        "fill": "hotpink",
-        "stroke": "purple",
-        "stroke-width": "8"
+        "x": 0,
+        "y": 0,
+        "width": GRAPHIC_WIDTH,
+        "height": GRAPHIC_HEIGHT,
+        "fill": GRAPHIC_BG_COLOR,
+        "stroke": "orangered",
+        "stroke-width": "8%",
+        "stroke-linecap": "round",
+        "stroke-dasharray": "1% 20%",
+        "stroke-dashoffset": "0%"
     }
 });
 node.addKeyframe({
     t: 1,
     attribs: {
-        "d": "M100,100 L200,200 L100,200Z",
-        "fill": "orange",
-        "stroke": "red",
-        "stroke-width": "0"
+        "x": 0,
+        "y": 0,
+        "width": GRAPHIC_WIDTH,
+        "height": GRAPHIC_HEIGHT,
+        "fill": GRAPHIC_BG_COLOR,
+        "stroke": "orangered",
+        "stroke-width": "8%",
+        "stroke-linecap": "round",
+        "stroke-dasharray": "1% 20%",
+        "stroke-dashoffset": "21%",
+        "border-radius": "5rem"
     }
 });
 
+const node2 = new GraphicNode("text");
+node2.addKeyframe({
+    t: 0,
+    attribs: {
+        "font-size": `${GRAPHIC_HEIGHT*(1/3)}px`,
+        "fill": "hsl(0,100%,50%)",
+    }
+});
+node2.addKeyframe({
+    t: 1/3,
+    attribs: {
+        "font-size": `${GRAPHIC_HEIGHT*(1/3)}px`,
+        "fill": "hsl(120,100%,50%)",
+    }
+});
+node2.addKeyframe({
+    t: 2/3,
+    attribs: {
+        "font-size": `${GRAPHIC_HEIGHT*(1/3)}px`,
+        "fill": "hsl(240,100%,50%)",
+    }
+});
+node2.addKeyframe({
+    t: 1,
+    attribs: {
+        "font-size": `${GRAPHIC_HEIGHT*(1/3)}px`,
+        "fill": "hsl(360,100%,50%)",
+    }
+});
+const text1 = new GraphicNode("tspan");
+text1.setTextContent("so");
+text1.addKeyframe({
+    t: 0,
+    attribs: {
+        "x": GRAPHIC_WIDTH*0.25,
+        "y": GRAPHIC_HEIGHT*(1/3),
+        "dy": `7%`
+    }
+});
+const text2 = new GraphicNode("tspan");
+text2.setTextContent("cool");
+text2.addKeyframe({
+    t: 0,
+    attribs: {
+        "x": GRAPHIC_WIDTH*0.25,
+        "y": GRAPHIC_HEIGHT*(1/3),
+        "dy": `${GRAPHIC_HEIGHT*(1/3)}px`
+    }
+})
+node2.appendChild(text1);
+node2.appendChild(text2);
+
 parentNode.appendChild(node);
+parentNode.appendChild(node2);
 graphic.addNode(parentNode);
 
 const startT = performance.now();
-graphic.render("SVG").then((blob) => {
-    console.log(blob);
-    const url = URL.createObjectURL(blob);
-    console.log(`render time: ${performance.now() - startT}`);
-    window.open(url);
-});
 
+graphic.render("SVG").then(handleRenderFinish);
 // graphic.render("GIF", async (_, blob) => {
 //     const url = URL.createObjectURL(blob);
 //     const img = new Image();
@@ -75,17 +129,49 @@ graphic.render("SVG").then((blob) => {
 //         (canvHeight * (aspectRatio >= 1)) + (canvWidth * (1/aspectRatio) * (aspectRatio > 1))
 //     );
 //     URL.revokeObjectURL(url);
-// })
-// .then((blob) => {
-//     console.log(blob);
-//     const gifUrl = URL.createObjectURL(blob);
-//     if (window.confirm("Download rendered GIF?")) {
-//         const a = document.createElement("a");
-//         a.href = gifUrl;
-//         a.download = "render.gif";
-//         a.click();
-//     } else {
-//         window.open(gifUrl);
-//     }
-//     URL.revokeObjectURL(gifUrl);
-// });
+// }).then(handleRenderFinish);
+
+function handleRenderFinish(blob) {
+    console.log(`Render Time: ${(performance.now() - startT)/1000} seconds`);
+    console.log(blob);
+    const confirmMessage = `Download rendered graphic? (${getFilesizeFromBlob(blob)})\nType: ${blob.type}`;
+    const url = URL.createObjectURL(blob);
+    if (window.confirm(confirmMessage)) {
+        const a = document.createElement("a");
+        a.href = url;
+        a.download = `rendered_graphic (${new Date().toLocaleString()}).${getExtFromBlob(blob)}`;
+        a.click();
+    } else {
+        window.open(url);
+    }
+    URL.revokeObjectURL(url);
+}
+
+function getFilesizeFromBlob(blob) {
+    const units = ["KB", "MB", "GB", "TB"];
+    let unit = "B";
+    let { size } = blob;
+    let nextUnitI = 0;
+    let temp;
+
+    while ((temp = size / 1000) >= 1) {
+        size = temp;
+        unit = units[Math.min(nextUnitI, units.length-1)];
+        nextUnitI++;
+    }
+
+    return `${size.toFixed(1)} ${unit}`;
+}
+
+function getExtFromBlob(blob) {
+    const regex = /^[A-Z]$/i;
+    const { type } = blob;
+    const startI = type.indexOf("/")+1;
+    let endI = type.length;
+    for (let i=startI; i<type.length; i++) {
+        if (regex.test(type[i])) continue;
+        endI = i;
+        break;
+    }
+    return type.substring(startI, endI);
+}
